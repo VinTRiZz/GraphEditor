f1ba37a7b08427f13e7e41725fbeaba08f15f94b
VinTRiZz <andzig501@gmail.com>
2025-07-29 02:16:41 +0300

    Багфикс скрипта создания AppImage

--- Deploy/createAppImage.sh
+++ Deploy/createAppImage.sh
@@ -14,7 +14,6 @@ fi
 
 # Configure for building
 cd "$CAI_SCRIPTDIR"
-cp -r "$CAI_SCRIPTDIR/Configurations/AppImage/usr" "$CAI_BUILDS_DIR"
 
 # Dir to contain App image
 APPIMAGE_DIR="Results/$(date +%H%M_%d%m%Y)"
@@ -22,14 +21,12 @@ mkdir $APPIMAGE_DIR
 cd $APPIMAGE_DIR
 
 # Setup binaries
-mkdir "$CAI_BUILDS_DIR/usr/bin"
 CURRENT_PROJECT_VERSION=$(grep -o 'VERSION \([0-9]\.\)\{2,\}.*' "$CAI_SCRIPTDIR/../CMakeLists.txt")
 CURRENT_PROJECT_VERSION=${CURRENT_PROJECT_VERSION/VERSION /}
-cp "$CAI_BUILDS_DIR/GraphEditor_$CURRENT_PROJECT_VERSION.bin" "$CAI_BUILDS_DIR/usr/bin/GraphEditor.bin"
+
 cp -r "$CAI_BUILDS_DIR/../lib/" ../
 cp "$CAI_SCRIPTDIR/../DATA/images/icons/app/GraphEditor.png" "$CAI_BUILDS_DIR/GraphEditor.png"
-sed "s/^Exec\=.*/Exec=GraphEditor-$CURRENT_PROJECT_VERSION.bin/" "$CAI_SCRIPTDIR/../DATA/desktop/GraphEditor.desktop" > "$CAI_BUILDS_DIR/PREPARE_GraphEditor.desktop"
-sed "s/^Name\=.*/Name=GraphEditor-$CURRENT_PROJECT_VERSION/" -i "$CAI_BUILDS_DIR/PREPARE_GraphEditor.desktop"
+sed "s/^Exec\=.*/Exec=GraphEditor-$CURRENT_PROJECT_VERSION.bin/; s/^Name\=.*/Name=GraphEditor-$CURRENT_PROJECT_VERSION/" "$CAI_SCRIPTDIR/../DATA/desktop/GraphEditor.desktop" > "$CAI_BUILDS_DIR/PREPARE_GraphEditor.desktop"
 
 # Process deploy
 "$CAI_SCRIPTDIR/tools/linuxdeploy-x86_64.AppImage" --appdir "$CAI_BUILDS_DIR" --output appimage -e "$CAI_BUILDS_DIR/GraphEditor-$CURRENT_PROJECT_VERSION.bin" --icon-file="$CAI_BUILDS_DIR/GraphEditor.png" --desktop-file="$CAI_BUILDS_DIR/PREPARE_GraphEditor.desktop"

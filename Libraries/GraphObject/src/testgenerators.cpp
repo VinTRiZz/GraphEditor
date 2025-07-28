5bb0396ac97599c36ffe66872ffb9c505d1f5498
VinTRiZz <andzig501@gmail.com>
2025-07-28 23:12:50 +0300

    Добавил генераторы полностью случайных графов

--- Libraries/GraphObject/src/testgenerators.cpp
+++ Libraries/GraphObject/src/testgenerators.cpp
@@ -1,7 +1,120 @@
 #include "testgenerators.h"
 
+#include <random>
+#include <ctime>
+#include <QColor>
+#include <QImage>
+#include <QJsonArray>
+#include <QJsonObject>
+
 namespace Graph::TestGenerators
 {
+
+// Генератор случайных строк
+QString randomString(int minLength = 3, int maxLength = 20) {
+    static const char alphanum[] = "0123456789"
+                                   "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                                   "abcdefghijklmnopqrstuvwxyz";
+    static std::mt19937 rng(std::time(nullptr));
+
+    std::uniform_int_distribution<int> lenDist(minLength, maxLength);
+    std::uniform_int_distribution<int> charDist(0, sizeof(alphanum)-2);
+
+    int length = lenDist(rng);
+    QString result;
+    result.reserve(length);
+
+    for (int i = 0; i < length; ++i) {
+        result.append(alphanum[charDist(rng)]);
+    }
+
+    return result;
+}
+
+// Генератор случайных цветов
+QColor randomColor(bool withAlpha = false) {
+    static std::mt19937 rng(std::time(nullptr));
+    std::uniform_int_distribution<int> dist(0, 255);
+
+    return QColor(dist(rng), dist(rng), dist(rng),
+                  withAlpha ? dist(rng) : 255);
+}
+
+// Генератор случайных изображений
+QImage randomImage() {
+    static std::mt19937 rng(std::time(nullptr));
+    std::uniform_int_distribution<int> sizeDist(16, 64);
+    std::uniform_int_distribution<int> colorDist(0, 255);
+    std::uniform_int_distribution<int> alphaDist(50, 255);
+
+    int size = sizeDist(rng);
+    QImage image(size, size, QImage::Format_ARGB32);
+
+    for (int y = 0; y < size; ++y) {
+        for (int x = 0; x < size; ++x) {
+            int alpha = (x == 0 || y == 0 || x == size-1 || y == size-1)
+                    ? 255 : alphaDist(rng);
+            image.setPixelColor(x, y, QColor(
+                                    colorDist(rng),
+                                    colorDist(rng),
+                                    colorDist(rng),
+                                    alpha
+                                    ));
+        }
+    }
+
+    return image;
+}
+
+// Генератор случайных JSON-объектов
+QJsonObject randomJsonObject(int maxDepth = 2) {
+    static std::mt19937 rng(std::time(nullptr));
+    std::uniform_int_distribution<int> typeDist(0, 5);
+    std::uniform_int_distribution<int> countDist(1, 5);
+    std::uniform_real_distribution<double> numDist(-100.0, 100.0);
+    std::uniform_int_distribution<int> boolDist(0, 1);
+
+    QJsonObject obj;
+    if (maxDepth <= 0) return obj;
+
+    int itemCount = countDist(rng);
+    for (int i = 0; i < itemCount; ++i) {
+        QString key = "prop_" + randomString(3, 8);
+
+        switch (typeDist(rng)) {
+        case 0: // String
+            obj[key] = randomString(2, 15);
+            break;
+        case 1: // Number
+            obj[key] = numDist(rng);
+            break;
+        case 2: // Boolean
+            obj[key] = boolDist(rng) == 1;
+            break;
+        case 3: // Array
+        {
+            QJsonArray arr;
+            int arrSize = countDist(rng);
+            for (int j = 0; j < arrSize; ++j) {
+                arr.append(numDist(rng));
+            }
+            obj[key] = arr;
+        }
+            break;
+        case 4: // Nested object
+            if (maxDepth > 1) {
+                obj[key] = randomJsonObject(maxDepth - 1);
+            }
+            break;
+        case 5: // Null
+            obj[key] = QJsonValue::Null;
+            break;
+        }
+    }
+
+    return obj;
+}
+
 
 PMaintainer createTestGraph()
 {
@@ -130,6 +243,91 @@ PMaintainer createTestGraph()
     for (auto& con : testConnections) {
         testGeneratedGraph->getObject().addConnection(con);
     }
+    return testGeneratedGraph;
+}
+
+
+Graph::GVertex randomVertex(GraphCommon::graphId_t minId,
+                            GraphCommon::graphId_t maxId) {
+    static std::mt19937 rng(std::time(nullptr));
+    std::uniform_real_distribution<double> posDist(0.0, 1000.0);
+    std::uniform_int_distribution<GraphCommon::graphId_t> idDist(minId, maxId);
+
+    Graph::GVertex vertex;
+    vertex.id = idDist(rng);
+    vertex.posX = posDist(rng);
+    vertex.posY = posDist(rng);
+    vertex.shortName = randomString(1, Graph::GRAPH_MAX_SHORTNAME_SIZE);
+    vertex.name = randomString(5, 50);
+    vertex.description = randomString(0, 200);
+    vertex.borderColor = randomColor(true);
+    vertex.backgroundColor = randomColor(true);
+    vertex.image = randomImage();
+    vertex.customProperties = randomJsonObject();
+
+    return vertex;
+}
+
+// Генератор случайных соединений
+Graph::GConnection randomConnection(
+        const std::vector<GraphCommon::graphId_t>& existingIds,
+        bool allowSelfConnect)
+{
+    static std::mt19937 rng(std::time(nullptr));
+
+    if (existingIds.size() < 2) {
+        throw std::invalid_argument("Need at least 2 vertices for connections");
+    }
+
+    std::uniform_real_distribution<double> weightDist(0.0, 100.0);
+    std::uniform_int_distribution<size_t> indexDist(0, existingIds.size()-1);
+
+    Graph::GConnection conn;
+
+    // Выбираем две разные вершины
+    size_t fromIdx = indexDist(rng);
+    size_t toIdx;
+    do {
+        toIdx = indexDist(rng);
+    } while (!allowSelfConnect && fromIdx == toIdx);
+
+    conn.idFrom = existingIds[fromIdx];
+    conn.idTo = existingIds[toIdx];
+    conn.connectionWeight = weightDist(rng);
+    conn.name = randomString(2, 30);
+    conn.lineColor = randomColor(true);
+    conn.customProperties = randomJsonObject();
+
+    return conn;
+}
+
+PMaintainer createRandomGraph()
+{
+    auto testGeneratedGraph = Graph::GraphMaintainer::createInstance();
+
+    testGeneratedGraph->setName(randomString());
+    testGeneratedGraph->setDescription(randomString());
+    testGeneratedGraph->setCreateTime(QDateTime::currentDateTime());
+    testGeneratedGraph->setEditTime(QDateTime::currentDateTime());
+
+    testGeneratedGraph->setCustomValue(randomString(), randomString());
+    testGeneratedGraph->setCustomValue(randomString(), randomString());
+
+    auto& targetGraph = testGeneratedGraph->getObject();
+    for (int i = 0; i < 100; ++i) {
+        targetGraph.addVertex(randomVertex());
+    }
+
+    auto allVertices = targetGraph.getAllVertices();
+    std::vector<GraphCommon::graphId_t> existingVertices;
+    existingVertices.reserve(allVertices.size());
+    for (auto& vert : allVertices) {
+        existingVertices.push_back(vert.id);
+    }
+    for (int i = 0; i < 30; ++i) {
+        targetGraph.addConnection(randomConnection(existingVertices));
+    }
+
     return testGeneratedGraph;
 }
 
--- Libraries/GraphObject/src/testgenerators.h
+++ Libraries/GraphObject/src/testgenerators.h
@@ -12,6 +12,31 @@ namespace Graph::TestGenerators
  */
 PMaintainer createTestGraph();
 
+/**
+ * @brief randomVertex  Создаёт полностью случайную вершину графа
+ * @param minId         Минимальный разрешённый ID
+ * @param maxId         Максимальный разрешённый ID
+ * @return              Случайная вершина
+ */
+Graph::GVertex randomVertex(GraphCommon::graphId_t minId = 1,
+                            GraphCommon::graphId_t maxId = 1000);
+
+/**
+ * @brief randomConnection  Создаёт полностью случайное соединение
+ * @param existingIds       Имеющиеся ID вершин
+ * @param allowSelfConnect  Флаг разрешения соединять вершины сами на себя
+ * @return                  Случайное соединение
+ */
+Graph::GConnection randomConnection(
+        const std::vector<GraphCommon::graphId_t>& existingIds,
+        bool allowSelfConnect = false);
+
+/**
+ * @brief createRandomGraph Создаёт граф с совершенно случайными данными, каждый раз разный
+ * @return                  Случайный граф
+ */
+PMaintainer createRandomGraph();
+
 }
 
 #endif // TESTGENERATORS_H

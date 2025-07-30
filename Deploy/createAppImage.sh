83e7579a5b00503847b0f78b85666e4206f46602
VinTRiZz <andzig501@gmail.com>
2025-07-30 23:16:53 +0300

    Исправил баги скрипта создания AppImage

--- Deploy/createAppImage.sh
+++ Deploy/createAppImage.sh
@@ -1,10 +1,13 @@
 #! /bin/bash
-CAI_SCRIPTDIR=$(realpath $(pwd))
-CAI_BUILDS_DIR="$CAI_SCRIPTDIR/../BIN/Package"
+CREATEAPPIMAGE_SCRIPTDIR=$(realpath $(dirname "$0"))
+CREATEAPPIMAGE_BUILDS_DIR="$CREATEAPPIMAGE_SCRIPTDIR/../BIN/Package"
+
+rm -r "$CREATEAPPIMAGE_SCRIPTDIR/../build" &> /dev/null
+rm -r "$CREATEAPPIMAGE_SCRIPTDIR/../BIN" &> /dev/null
 
 # Build install target
-mkdir ../build &> /dev/null
-cd ../build && cmake .. -DRELEASE=1
+mkdir "$CREATEAPPIMAGE_SCRIPTDIR/../build" &> /dev/null
+cd "$CREATEAPPIMAGE_SCRIPTDIR/../build" && cmake ..
 cmake --build . --parallel --target install
 
 if [[ "$?" != "0" ]]; then
@@ -13,21 +16,24 @@ if [[ "$?" != "0" ]]; then
 fi
 
 # Configure for building
-cd "$CAI_SCRIPTDIR"
+cd "$CREATEAPPIMAGE_SCRIPTDIR"
+
+# Setup binaries
+CURRENT_PROJECT_VERSION=$(grep -o "VERSION \\([0-9]\\{1,\}\\.\\)\\{2,\\}.*" "$CREATEAPPIMAGE_SCRIPTDIR/../CMakeLists.txt")
+CURRENT_PROJECT_VERSION=${CURRENT_PROJECT_VERSION//VERSION /}
 
 # Dir to contain App image
-APPIMAGE_DIR="Results/$(date +%H%M_%d%m%Y)"
+APPIMAGE_DIR="$CREATEAPPIMAGE_SCRIPTDIR/Results/$(date +%H%M_%d%m%Y)"
 mkdir $APPIMAGE_DIR
 cd $APPIMAGE_DIR
 
-# Setup binaries
-CURRENT_PROJECT_VERSION=$(grep -o 'VERSION \([0-9]\.\)\{2,\}.*' "$CAI_SCRIPTDIR/../CMakeLists.txt")
-CURRENT_PROJECT_VERSION=${CURRENT_PROJECT_VERSION/VERSION /}
-
-cp -r "$CAI_BUILDS_DIR/../lib/" ../
-cp "$CAI_SCRIPTDIR/../DATA/images/icons/app/GraphEditor.png" "$CAI_BUILDS_DIR/GraphEditor.png"
-sed "s/^Exec\=.*/Exec=GraphEditor-$CURRENT_PROJECT_VERSION.bin/; s/^Name\=.*/Name=GraphEditor-$CURRENT_PROJECT_VERSION/" "$CAI_SCRIPTDIR/../DATA/desktop/GraphEditor.desktop" > "$CAI_BUILDS_DIR/PREPARE_GraphEditor.desktop"
+cp -r "$CREATEAPPIMAGE_BUILDS_DIR/../lib/" ../
+cp "$CREATEAPPIMAGE_SCRIPTDIR/../DATA/images/icons/app/GraphEditor.png" "$CREATEAPPIMAGE_BUILDS_DIR/GraphEditor.png"
+sed "s/^Exec\=.*/Exec=GraphEditor-$CURRENT_PROJECT_VERSION.bin/; s/^Name\=.*/Name=GraphEditor-$CURRENT_PROJECT_VERSION/" "$CREATEAPPIMAGE_SCRIPTDIR/../DATA/desktop/GraphEditor.desktop" > "$CREATEAPPIMAGE_BUILDS_DIR/PREPARE_GraphEditor.desktop"
 
 # Process deploy
-"$CAI_SCRIPTDIR/tools/linuxdeploy-x86_64.AppImage" --appdir "$CAI_BUILDS_DIR" --output appimage -e "$CAI_BUILDS_DIR/GraphEditor-$CURRENT_PROJECT_VERSION.bin" --icon-file="$CAI_BUILDS_DIR/GraphEditor.png" --desktop-file="$CAI_BUILDS_DIR/PREPARE_GraphEditor.desktop"
+"$CREATEAPPIMAGE_SCRIPTDIR/tools/linuxdeploy-x86_64.AppImage" --appdir "$CREATEAPPIMAGE_BUILDS_DIR" --output appimage -e "$CREATEAPPIMAGE_BUILDS_DIR/GraphEditor-$CURRENT_PROJECT_VERSION.bin" --icon-file="$CREATEAPPIMAGE_BUILDS_DIR/GraphEditor.png" --desktop-file="$CREATEAPPIMAGE_BUILDS_DIR/PREPARE_GraphEditor.desktop"
 rm -r ../lib
+
+# Вернёмся откуда пришли
+cd -

32e0956cd7c5598affcd28f88d36ba8599bcd726
VinTRiZz <andzig501@gmail.com>
2025-08-04 23:35:17 +0300

    Избавился от нужды в Xcb plugin

--- Deploy/createAppImage.sh
+++ Deploy/createAppImage.sh
@@ -2,38 +2,47 @@
 CREATEAPPIMAGE_SCRIPTDIR=$(realpath $(dirname "$0"))
 CREATEAPPIMAGE_BUILDS_DIR="$CREATEAPPIMAGE_SCRIPTDIR/../BIN/Package"
 
+# Очистка созданного ранее билда (для гарантий, что не использовался кэш)
 rm -r "$CREATEAPPIMAGE_SCRIPTDIR/../build" &> /dev/null
 rm -r "$CREATEAPPIMAGE_SCRIPTDIR/../BIN" &> /dev/null
 
-# Build install target
+# Настройка проекта
 mkdir "$CREATEAPPIMAGE_SCRIPTDIR/../build" &> /dev/null
-cd "$CREATEAPPIMAGE_SCRIPTDIR/../build" && cmake ..
-cmake --build . --parallel --target install
+cd "$CREATEAPPIMAGE_SCRIPTDIR/../build"
 
+# Сборка проекта
+if (command -v ninja); then
+    cmake -GNinja .. && ninja install
+else
+    cmake .. && cmake --build . --parallel --target install
+fi
+
+# Проверка, собралось ли
 if [[ "$?" != "0" ]]; then
     echo "Error building install target"
     exit 1
 fi
 
-# Configure for building
+# Перемещаемся в директорию скрипта
 cd "$CREATEAPPIMAGE_SCRIPTDIR"
 
-# Setup binaries
+# Поиск версии проекта
 CURRENT_PROJECT_VERSION=$(grep -o "VERSION \\([0-9]\\{1,\}\\.\\)\\{2,\\}.*" "$CREATEAPPIMAGE_SCRIPTDIR/../CMakeLists.txt")
 CURRENT_PROJECT_VERSION=${CURRENT_PROJECT_VERSION//VERSION /}
 
-# Dir to contain App image
+# Директория с AppImage в результате
 APPIMAGE_DIR="$CREATEAPPIMAGE_SCRIPTDIR/Results/$(date +%H%M_%d%m%Y)"
 mkdir $APPIMAGE_DIR
 cd $APPIMAGE_DIR
 
+# Копирование библиотек проекта
 cp -r "$CREATEAPPIMAGE_BUILDS_DIR/../lib/" ../
 cp "$CREATEAPPIMAGE_SCRIPTDIR/../DATA/images/icons/app/GraphEditor.png" "$CREATEAPPIMAGE_BUILDS_DIR/GraphEditor.png"
 sed "s/^Exec\=.*/Exec=GraphEditor-$CURRENT_PROJECT_VERSION.bin/; s/^Name\=.*/Name=GraphEditor-$CURRENT_PROJECT_VERSION/" "$CREATEAPPIMAGE_SCRIPTDIR/../DATA/desktop/GraphEditor.desktop" > "$CREATEAPPIMAGE_BUILDS_DIR/PREPARE_GraphEditor.desktop"
 
-# Process deploy
-"$CREATEAPPIMAGE_SCRIPTDIR/tools/linuxdeploy-x86_64.AppImage" --appdir "$CREATEAPPIMAGE_BUILDS_DIR" --output appimage -e "$CREATEAPPIMAGE_BUILDS_DIR/GraphEditor-$CURRENT_PROJECT_VERSION.bin" --icon-file="$CREATEAPPIMAGE_BUILDS_DIR/GraphEditor.png" --desktop-file="$CREATEAPPIMAGE_BUILDS_DIR/PREPARE_GraphEditor.desktop"
-rm -r ../lib
+# Для того, чтобы linuxdeploy нашёл плагин Qt
+set PATH "$PATH:$CREATEAPPIMAGE_SCRIPTDIR/tools"
 
-# Вернёмся откуда пришли
-cd -
+# Создание непосредственно AppImage
+"$CREATEAPPIMAGE_SCRIPTDIR/tools/linuxdeploy-x86_64.AppImage" --plugin qt --appdir "$CREATEAPPIMAGE_BUILDS_DIR" --output appimage -e "$CREATEAPPIMAGE_BUILDS_DIR/GraphEditor-$CURRENT_PROJECT_VERSION.bin" --icon-file="$CREATEAPPIMAGE_BUILDS_DIR/GraphEditor.png" --desktop-file="$CREATEAPPIMAGE_BUILDS_DIR/PREPARE_GraphEditor.desktop"
+rm -r ../lib

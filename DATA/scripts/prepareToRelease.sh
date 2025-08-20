983a170c5d8fa6fd5895d190ee5cc4818969ea08
VinTRiZz <andzig501@gmail.com>
2025-08-20 23:09:57 +0300

    Исправил что забыл в скрипте

--- DATA/scripts/prepareToRelease.sh
+++ DATA/scripts/prepareToRelease.sh
@@ -3,20 +3,18 @@
 PRPTC_SCRIPTDIR=$(realpath "$(dirname $0)")
 
 cd "$PRPTC_SCRIPTDIR/../.."
-#rm -r build BIN &> /dev/null
-# mkdir build && cd build
+rm -r build BIN &> /dev/null
+mkdir build && cd build
 if [ "$?" != "0" ]; then
     echo "Did not created builds dir"
     exit 1
 fi
 
-#cmake -GNinja .. && cmake --build . --target all
+cmake -GNinja .. && cmake --build . --target all
 if [ "$?" != "0" ]; then
     echo "Error building the project"
     exit 2
 fi
-
-doxygen "$PRPTC_SCRIPTDIR/../etc/Doxyfile"
 
 cd "$PRPTC_SCRIPTDIR/../etc"
 clang-format-19 -i $(find "$PRPTC_SCRIPTDIR/../.." -iname '*.cpp' -o -iname '*.hpp')
